bbfb3e527b58e6ca210327789246ac26a2bcd98b
taylorafox1 <taylorafox1@gmail.com>
2019-02-16 14:07:41 -0500

    added app layout wrapper

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import WelcomeMessage from './WelcomeMessage';
+import AppLayout from './AppLayout';
+
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <WelcomeMessage />
+      </AppLayout>
+    );
+  }
+}
+
+export default App;
