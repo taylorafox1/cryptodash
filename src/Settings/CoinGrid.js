e2c190a3079dd0df3388abbbde11df6432fa1de1
taylorafox1 <taylorafox1@gmail.com>
2019-02-22 14:56:51 -0500

    added prices and fuzzy search

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -6,19 +6,24 @@ import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
 	display: grid;
-	grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
+	grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
 	grid-gap: 15px;
 	margin-top: 40px;
 `
 
-function getCoinsToDisplay(coinList, topSection, favorites){
-	return topSection ? favorites : Object.keys(coinList).slice(0, 100);
+function getLowerSectionCoins(coinList, filteredCoins) {
+	return (filteredCoins && Object.keys(filteredCoins)) || 
+			Object.keys(coinList).slice(0, 100);
+}
+
+function getCoinsToDisplay(coinList, topSection, favorites, filteredCoins){
+	return topSection ? favorites : getLowerSectionCoins(coinList, filteredCoins);
 }
 
 export default function({topSection}) {
 	return <AppContext.Consumer>
-		{({ coinList, favorites }) => <CoinGridStyled>
-		{getCoinsToDisplay(coinList, topSection, favorites).map(coinKey =>
+		{({ coinList, favorites, filteredCoins }) => <CoinGridStyled>
+		{getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey =>
 			<CoinTile topSection={topSection} coinKey={coinKey} />
 			)}
 		</CoinGridStyled>}
