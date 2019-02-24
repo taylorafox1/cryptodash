1b08bb07d2b99e5f761eca59a9c6f174d1be39d7
taylorafox1 <taylorafox1@gmail.com>
2019-02-24 15:34:45 -0500

    Add Historical Data

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,51 +1,116 @@
 import React from 'react';
 import styled, {css} from 'styled-components';
-import {AppContext} from './AppProvider'
+import {AppContext} from "./AppProvider";
 
 const Logo = styled.div`
-	font-size: 1.5em;
+  font-size: 1.5em; 
 `
 
-
 const Bar = styled.div`
-	display: grid;
-	margin-bottom: 40px;
-	grid-template-columns: 180px auto 100px 100px;
+  display: grid; 
+  margin-bottom: 40px; 
+  grid-template-columns: 180px auto 100px 100px; 
 `
 
 const ControlButtonElem = styled.div`
-	cursor: pointer;
-	${props => props.active && css`
-		text-shadow: 0px 0px 60px #03ff03;
-	`}
+  cursor: pointer; 
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+  `}
+  ${props => props.hidden && css`
+    display: none; 
+  `}
 `
 
 function toProperCase(lower){
-	return lower.charAt(0).toUpperCase() + lower.substr(1);
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
 }
 
-function ControlButton({ name, active }){
-	return (
-		<AppContext.Consumer>
-			{({page, setPage}) => (
-			<ControlButtonElem 
-				active={ page === name } 
-				onClick={() => setPage(name)}
-			> 
-				{ toProperCase(name) }
-			</ControlButtonElem>
-			)}
-		</AppContext.Consumer>
-	)
+function ControlButton({name}){
+  return (
+    <AppContext.Consumer>
+      {({firstVisit, page, setPage}) => (
+        <ControlButtonElem
+          active={page === name}
+          onClick={()=> setPage(name)}
+          hidden={firstVisit && name === 'dashboard'}
+        >
+          {toProperCase(name)}
+        </ControlButtonElem>
+        )}
+    </AppContext.Consumer>
+  )
 }
 
 export default function(){
-	return (
-	<Bar> 
-		<Logo> CryptoDash </Logo>
-		<div />
-		<ControlButton active name="dashboard" />  
-		<ControlButton name="settings" />
-	</Bar>
-	)
+  return (
+    <Bar>
+      <Logo> CryptoDash </Logo>
+      <div/>
+      <ControlButton active name="dashboard"/>
+      <ControlButton name="settings"/>
+    </Bar>
+  );
 }
+
+
+
+
+
+
+
+// import React from 'react';
+// import styled, {css} from 'styled-components';
+// import {AppContext} from './AppProvider'
+
+// const Logo = styled.div`
+// 	font-size: 1.5em;
+// `
+
+
+// const Bar = styled.div`
+// 	display: grid;
+// 	margin-bottom: 40px;
+// 	grid-template-columns: 180px auto 100px 100px;
+// `
+
+// const ControlButtonElem = styled.div`
+// 	cursor: pointer;
+// 	${props => props.active && css`
+// 		text-shadow: 0px 0px 60px #03ff03;
+// 	`}
+// 	${props => props.hidden && css`
+// 		display: none;
+// 		`}
+// `
+
+// function toProperCase(lower){
+// 	return lower.charAt(0).toUpperCase() + lower.substr(1);
+// }
+
+// function ControlButton({ name, active }){
+// 	return (
+// 		<AppContext.Consumer>
+// 			{({firstVisit, page, setPage}) => (
+// 			<ControlButtonElem 
+// 				active={ page === name } 
+// 				onClick={() => setPage(name)}
+// 				hidden={firstVisit && name === 'dashboard'}
+// 			> 
+// 				{ toProperCase(name) }
+// 			</ControlButtonElem>
+// 			)}
+// 		</AppContext.Consumer>
+// 	)
+// }
+
+// export default function(){
+// 	return (
+// 	<Bar> 
+// 		<Logo> CryptoDash </Logo>
+// 		<div />
+// 		<ControlButton active name="dashboard" />  
+// 		<ControlButton name="settings" />
+// 	</Bar>
+// 	)
+// }
--- src/Dashboard/PriceChart.js
+++ src/Dashboard/PriceChart.js
@@ -10,9 +10,12 @@ export default function() {
 	return (
 		<AppContext.Consumer>
 			{({historical}) =>
-			<Tile>
-				<ReactHighcharts config={highchartsConfig(historical)} />
-			</Tile>
+				<Tile>
+					{historical ? 
+						<ReactHighcharts config={highchartsConfig(historical)} />
+						: <div>Loading Historical Data</div>
+					}
+				</Tile>
 			}
 		</AppContext.Consumer>
 	)
