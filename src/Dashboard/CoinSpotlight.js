b95f5d7fe8bd3ae1234db90396876a9172aafba1
taylorafox1 <taylorafox1@gmail.com>
2019-02-24 13:22:04 -0500

    Add Spotlight Coin

---
+++ src/Dashboard/CoinSpotlight.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import {Tile} from "../Shared/Tile";
+// import {fontSize3, fontSizeBig, greenBoxShadow} from '../Shared/Styles';
+// import {CoinHeaderGridStyled} from '../Settings/CoinHeaderGrid';
+import {AppContext} from '../App/AppProvider';
+import CoinImage from '../Shared/CoinImage';
+
+const SpotlightName = styled.h2`
+	text-align: center;
+`
+
+export default function() {
+	return (
+		<AppContext.Consumer>
+			{({currentFavorite, coinList}) =>
+			<Tile> 
+				<SpotlightName> {coinList[currentFavorite].CoinName} </SpotlightName>
+				<CoinImage spotlight coin={coinList[currentFavorite]}/>
+			</Tile>
+			}
+		</AppContext.Consumer>
+	)
+}
